021491897809914f530d2b1cb6b994f9b5b1298f
PatrickMontes <alexandermontes30p@hotmail.com>
2024-09-27 21:43:35 -0500

    KafkaController Hecho

--- src/main/java/cibertec/edu/pe/daw_Examen_T1_Kafka/controller/KafkaController.java
+++ src/main/java/cibertec/edu/pe/daw_Examen_T1_Kafka/controller/KafkaController.java
@@ -1,4 +1,22 @@
 package cibertec.edu.pe.daw_Examen_T1_Kafka.controller;
 
+import cibertec.edu.pe.daw_Examen_T1_Kafka.kafka.KafkaProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class KafkaController {
+
+    private final KafkaProducer kafkaProducer;
+
+
+    @GetMapping("/enviar")
+    public String enviarNotificacion(@RequestParam String mensaje){
+        kafkaProducer.enviarMensajeNotificacion(mensaje);
+        return "Mensaje enviado a Kafka";
+    }
+
 }
