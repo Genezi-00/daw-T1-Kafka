153fe8f8281b668ff17e2778d4a920b8b7ed5460
Natha23XO <hugocastroleon2301@gmail.com>
2024-09-27 21:23:36 -0500

    avanzando producer y consumer

--- src/main/java/cibertec/edu/pe/daw_Examen_T1_Kafka/kafka/KafkaConsumer.java
+++ src/main/java/cibertec/edu/pe/daw_Examen_T1_Kafka/kafka/KafkaConsumer.java
@@ -1,4 +1,15 @@
 package cibertec.edu.pe.daw_Examen_T1_Kafka.kafka;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class KafkaConsumer {
+
+    @KafkaListener(topics = "topic_cibertec-app-examen-daw",groupId = "group_cibertec-examen-daw")
+    public void obtenerMensajeNotificacion(String mensaje){
+        log.info("Mensaje de notificacion de orden recibido "+mensaje);
+    }
 }
--- src/main/java/cibertec/edu/pe/daw_Examen_T1_Kafka/kafka/KafkaProducer.java
+++ src/main/java/cibertec/edu/pe/daw_Examen_T1_Kafka/kafka/KafkaProducer.java
@@ -1,4 +1,20 @@
 package cibertec.edu.pe.daw_Examen_T1_Kafka.kafka;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
 public class KafkaProducer {
+
+    private final KafkaTemplate<String,String> kafkaTemplate;
+    private static final String TOPIC = "topic_cibertec-app-examen-daw";
+
+    public void enviarMensajeNotificacion(String mensaje){
+        kafkaTemplate.send(TOPIC,mensaje);
+        log.info("Mensaje construido");
+    }
 }
